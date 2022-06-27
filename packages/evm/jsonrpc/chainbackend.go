daf90ef306e4c16d267c611e7dd7814a8069af70
Lukas Möller <lukas.moeller@iota.org>
2022-06-27 14:01:45 +0200

    Fix broken evm backend merge

--- packages/evm/jsonrpc/chainbackend.go
+++ packages/evm/jsonrpc/chainbackend.go
@@ -6,18 +6,10 @@ package jsonrpc
 import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/iotaledger/wasp/packages/cryptolib"
-	"github.com/iotaledger/wasp/packages/iscp"
 	"github.com/iotaledger/wasp/packages/kv/dict"
 )
 
 type ChainBackend interface {
-	EstimateGasOnLedger(scName string, funName string, transfer *iscp.FungibleTokens, args dict.Dict) (uint64, *iscp.FungibleTokens, error)
-	PostOnLedgerRequest(scName string, funName string, transfer *iscp.FungibleTokens, args dict.Dict, gasBudget uint64) error
-	EstimateGasOffLedger(scName string, funName string, args dict.Dict) (uint64, *iscp.FungibleTokens, error)
-	PostOffLedgerRequest(scName string, funName string, args dict.Dict, gasBudget uint64) error
-	CallView(scName string, funName string, args dict.Dict) (dict.Dict, error)
-	Signer() cryptolib.VariantKeyPair
 	EVMSendTransaction(tx *types.Transaction) error
 	EVMEstimateGas(callMsg ethereum.CallMsg) (uint64, error)
 	ISCCallView(scName string, funName string, args dict.Dict) (dict.Dict, error)
