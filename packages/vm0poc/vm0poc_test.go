f1e832b58687b33abad059c24092aa0d54d253ff
lunfardo314 <evaldas.drasutis@iota.org>
2022-07-14 15:33:02 +0300

    updated vm0poc tests with assertions on the state

--- packages/vm0poc/vm0poc_test.go
+++ packages/vm0poc/vm0poc_test.go
@@ -4,16 +4,18 @@ import (
 	"testing"
 
 	"github.com/iotaledger/wasp/packages/solo"
+	"github.com/iotaledger/wasp/packages/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBasic(t *testing.T) {
 	t.Run("create chain", func(t *testing.T) {
 		env := solo.New(t)
-		_ = env.NewChain(nil, "ch1", solo.InitChainOptions{
+		ch := env.NewChain(nil, "ch1", solo.InitChainOptions{
 			VMRunner:         NewVMRunner(),
 			BypassStardustVM: true,
 		})
+		require.False(t, ch.HasRaw([]byte(CounterStateVar)))
 	})
 	t.Run("send 1 request", func(t *testing.T) {
 		env := solo.New(t)
@@ -24,6 +26,12 @@ func TestBasic(t *testing.T) {
 		req := solo.NewCallParams("dummy", "dummy", ParamDeltaInt64, int64(10))
 		_, err := ch.PostRequestOffLedger(req, nil)
 		require.NoError(t, err)
+
+		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		v := ch.GetRaw([]byte(CounterStateVar))
+		val, err := util.Int64From8Bytes(v)
+		require.NoError(t, err)
+		require.EqualValues(t, int64(10), val)
 	})
 	t.Run("send 5 requests", func(t *testing.T) {
 		env := solo.New(t)
@@ -35,6 +43,38 @@ func TestBasic(t *testing.T) {
 		for i := 0; i < 5; i++ {
 			_, err := ch.PostRequestOffLedger(req, nil)
 			require.NoError(t, err)
+			require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+			v := ch.GetRaw([]byte(CounterStateVar))
+			val, err := util.Int64From8Bytes(v)
+			require.NoError(t, err)
+			require.EqualValues(t, int64(i+1), val)
 		}
+	})
+	t.Run("send 2 requests", func(t *testing.T) {
+		env := solo.New(t)
+		ch := env.NewChain(nil, "ch1", solo.InitChainOptions{
+			VMRunner:         NewVMRunner(),
+			BypassStardustVM: true,
+		})
+		req := solo.NewCallParams("dummy", "dummy", ParamDeltaInt64, int64(10))
+		_, err := ch.PostRequestOffLedger(req, nil)
+		require.NoError(t, err)
+
+		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		v := ch.GetRaw([]byte(CounterStateVar))
+		val, err := util.Int64From8Bytes(v)
+		require.NoError(t, err)
+		require.EqualValues(t, int64(10), val)
+
+		req = solo.NewCallParams("dummy", "dummy", ParamDeltaInt64, int64(-5))
+		_, err = ch.PostRequestOffLedger(req, nil)
+		require.NoError(t, err)
+
+		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		v = ch.GetRaw([]byte(CounterStateVar))
+		val, err = util.Int64From8Bytes(v)
+		require.NoError(t, err)
+		require.EqualValues(t, int64(5), val)
+
 	})
 }
