e1e436e48fdb8c57b36c4e3941ff8f2434f3decf
lunfardo314 <evaldas.drasutis@iota.org>
2022-07-15 09:58:39 +0300

    updated vm0poc tests with assertions on the state

--- packages/vm0poc/vm0poc_test.go
+++ packages/vm0poc/vm0poc_test.go
@@ -15,7 +15,7 @@ func TestBasic(t *testing.T) {
 			VMRunner:         NewVMRunner(),
 			BypassStardustVM: true,
 		})
-		require.False(t, ch.HasRaw([]byte(CounterStateVar)))
+		require.False(t, ch.RawState().MustHas(CounterStateVar))
 	})
 	t.Run("send 1 request", func(t *testing.T) {
 		env := solo.New(t)
@@ -27,7 +27,7 @@ func TestBasic(t *testing.T) {
 		_, err := ch.PostRequestOffLedger(req, nil)
 		require.NoError(t, err)
 
-		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		require.True(t, ch.RawState().MustHas(CounterStateVar))
 		v := ch.GetRaw([]byte(CounterStateVar))
 		val, err := util.Int64From8Bytes(v)
 		require.NoError(t, err)
@@ -43,7 +43,7 @@ func TestBasic(t *testing.T) {
 		for i := 0; i < 5; i++ {
 			_, err := ch.PostRequestOffLedger(req, nil)
 			require.NoError(t, err)
-			require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+			require.True(t, ch.RawState().MustHas(CounterStateVar))
 			v := ch.GetRaw([]byte(CounterStateVar))
 			val, err := util.Int64From8Bytes(v)
 			require.NoError(t, err)
@@ -60,7 +60,7 @@ func TestBasic(t *testing.T) {
 		_, err := ch.PostRequestOffLedger(req, nil)
 		require.NoError(t, err)
 
-		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		require.True(t, ch.RawState().MustHas(CounterStateVar))
 		v := ch.GetRaw([]byte(CounterStateVar))
 		val, err := util.Int64From8Bytes(v)
 		require.NoError(t, err)
@@ -70,7 +70,7 @@ func TestBasic(t *testing.T) {
 		_, err = ch.PostRequestOffLedger(req, nil)
 		require.NoError(t, err)
 
-		require.True(t, ch.HasRaw([]byte(CounterStateVar)))
+		require.True(t, ch.RawState().MustHas(CounterStateVar))
 		v = ch.GetRaw([]byte(CounterStateVar))
 		val, err = util.Int64From8Bytes(v)
 		require.NoError(t, err)
