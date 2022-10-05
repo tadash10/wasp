90cb75578b2bc624642441a8925c76c0f7dc3b8f
Lukas Möller <lukas.moeller@iota.org>
2022-10-05 13:57:45 +0200

    Initial version of the evm toolkit frontend

---
+++ tools/evm-toolkit/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Withdraw from './Withdraw.svelte';
+  import { SvelteToast } from '@zerodevx/svelte-toast'
+</script>
+
+<main>  
+  <div class="item"><Withdraw /></div>
+  <div class="item"><Withdraw /></div>
+</main>
+<SvelteToast/>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .item {
+    justify-content: center;
+    justify-items: center;
+    flex-grow: 1;
+  }
+
+  @media (min-width: 1000px) {
+    main {
+      flex-direction: row;
+    }
+  }
+
+</style>
